c3fb819ab861701b9c602ec7aaba172517177364
KimGeonHee <kafoo0105@gmail.com>
2024-08-23 16:15:49 +0900

    [ADD]홈 화면

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -3,7 +3,9 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown'
-
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import 로고 from '../assets/images/로고.png';
 
 
 function Home() {
@@ -22,21 +24,34 @@ function Home() {
   return (
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+          {/* 로고 사진 */}
+          <Row>
+          <Col xs={6} md={4}>
+            <img src={로고} style={{ height: '24px', width: '17px', marginRight: '5px' }} />
+          </Col>
+          </Row>
+
+          <Navbar.Brand href="#home">
+            
+            RoomCatcher
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
+              <Nav.Link href="#intro">소개</Nav.Link>
+              <Nav.Link href="#type-description">유형 설명</Nav.Link>
+              <Nav.Link href="#subscription-info">청약 정보 글</Nav.Link>
+              <Nav.Link href="#mypage">마이페이지</Nav.Link>
+              <NavDropdown title="더보기" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#favorites">찜한 매물</NavDropdown.Item>
+                <NavDropdown.Item href="#recommended-properties">
+                  추천 매물
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Item href="#help">도움말</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
-                  Separated link
+                  로그아웃
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
